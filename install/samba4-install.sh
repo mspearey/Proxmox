751c24ed0619ea8e8942b50e526783d10abff9c7
Mathew Spearey <mathews@spearey.com>
2024-02-12 16:06:43 +0000

    temp changes  samba4-install.sh

--- install/samba4-install.sh
+++ install/samba4-install.sh
@@ -5,6 +5,21 @@
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
+kerb() {
+  ADMINTOKEN=''
+  if NEWTOKEN=$(whiptail --passwordbox "Setup your ADMIN_TOKEN (make it strong)" 10 58 3>&1 1>&2 2>&3); then
+    if [[ ! -z "$NEWTOKEN" ]]; then
+      ADMINTOKEN=$(echo -n ${NEWTOKEN} | argon2 "$(openssl rand -base64 32)" -e -id -k 19456 -t 2 -p 1)
+    else
+      clear
+      echo -e "⚠  User didn't setup ADMIN_TOKEN, admin panel is disabled! \n"
+    fi
+  else
+    clear
+    echo -e "⚠  User didn't setup ADMIN_TOKEN, admin panel is disabled! \n"
+  fi
+}
+
 source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
 color
 verb_ip6
@@ -23,9 +38,15 @@ $STD apt-get install -y acl \
  attr \
  winbind \
  libpam-winbind \
- libnss-winbind \
- krb5-config \
- krb5-user \
+ libnss-winbind
+
+# domain details
+kerb()
+
+# krb4-config shows interactive dialog. Can configure later.
+$STD DEBIAN_FRONTEND=noninteractive apt-get install -y krb5-config
+
+$STD apt-get install -y krb5-user \
  dnsutils \
  python3-setproctitle
 msg_ok "Installed Dependencies"
@@ -33,6 +54,28 @@ msg_ok "Installed Dependencies"
 msg_info "Installing Samba"
 $STD apt-get install -y samba
 msg_ok "Installed Samba"
+
+sed '/\[realms\]/ a\
+        COOP.SPEAREY.COM = { \
+                kdc = SambaDC.COOP.SPEAREY.COM \
+                admin_server = SambaDC.COOP.SPEAREY.COM \
+        }' /etc/krb5.conf
+###
+        workgroup = EXAMPLE 
+        realm = EXAMPLE.COM 
+        netbios name = SAMBA4 
+        server role= active directory domain controller 
+        dns forwarder = 8.8.8.8
+
+##
+[netlogon] 
+comment = Network Logon Service 
+path =/var/lib/samba/sysvol/example.com/scripts 
+read only = No 
+[sysvol] 
+comment = System Volume 
+path = /var/lib/samba/sysvol 
+read only = No
 
 mv /etc/samba/smb.conf /etc/samba/smb.conf.initial
 $STD samba-tool domain provision --use-rfc2307 --interactive
